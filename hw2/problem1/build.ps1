edb98de72caa320ee3d5af029f36dac77b7910c9
Leo J Robles <lroblesa@usc.edu>
2025-11-05 23:21:36 -0800

    Delete hw2 directory

--- hw2/problem1/build.ps1
+++
@@ -1,9 +0,0 @@
-﻿# build.ps1
-Write-Host "Building Docker image: arxiv-server:latest..."
-docker build -t arxiv-server:latest .
-if (0 -eq 0) {
-    Write-Host "Build completed succesfully"
-} else {
-    Write-Host "Error in build"
-    exit 0
-}
